719ec8fdcf12c663968502ec00f4355065758ff9
Ashley Salazar <ashleysalazar0902@gmail.com>
2021-04-22 16:05:32 -0500

    commit tree f9c3bb706d80308f68383f0ae4a7b781d54aace6

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+    return [drivers[0], drivers[1]]; 
+};
+
+const returnLastTwoDrivers = (drivers) => {
+    return [drivers[2], drivers[3]];
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (fair) => {
+    return function(){
+        return fair * fair
+    }
+}
+
+const fareDoubler = (money) => {
+   return money * 2;
+}
+
+const fareTripler = (moolah) => {
+    return moolah * 3;
+}
+
+const selectDifferentDrivers = (drivers, returnDrivers) => {
+    if (returnDrivers == returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(drivers);
+    } else {
+        return returnLastTwoDrivers(drivers);
+    }
+}
+
